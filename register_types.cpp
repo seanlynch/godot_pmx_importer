b1cae709de901fef4c6e2f43ce8cd94e4dab7708
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-09-05 14:00:39 -0700

    Cleanup.

--- register_types.cpp
+++ register_types.cpp
@@ -1,12 +1,8 @@
 #include "register_types.h"
 
 #include "core/class_db.h"
-#include "lasso.h"
-// #include "lasso_point.h"
 
 void register_pmx_types() {
-	ClassDB::register_class<LassoDB>();
-	ClassDB::register_class<LassoPoint>();
 }
 
 void unregister_pmx_types() {
